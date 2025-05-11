671aa83deebec0f258f3ecdc6b139d221f2d1c2b
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-11 15:09:26 +0530

    Circular Queue impl using Array

--- CompetitiveProgramming/src/QueueImplementationUsingArray.java
+++ CompetitiveProgramming/src/QueueImplementationUsingArray.java
@@ -4,24 +4,31 @@ public class QueueImplementationUsingArray {
         int[] s;
 
         Queue(int n) {
-            this.n = n;
+            this.n = n + 1;
             this.rear = 0;
             this.front = 0;
-            s = new int[n];
+            s = new int[this.n];
         }
 
         public void offer(int e) {
-            if (rear == n) throw new RuntimeException("queue is full!");
-            else s[rear++] = e;
+            if (front == (rear + 1) % n) throw new RuntimeException("queue is full!");
+            else {
+                s[rear] = e;
+                rear = (rear + 1) % n;
+            }
         }
 
         public int poll() {
             if (rear == front) throw new RuntimeException("queue is Empty!");
-            else return s[front++];
+            else {
+                int temp = s[front];
+                front = (front + 1) % n;
+                return temp;
+            }
         }
 
         public int size() {
-            return rear - front;
+            return (n - front + rear) % n;
         }
 
         public int peek() {
@@ -36,17 +43,19 @@ public class QueueImplementationUsingArray {
 
     public static void main(String[] args) {
         Queue q = new Queue(5);
-        q.offer(1);
-        q.offer(2);
-//        q.offer(3);
-//        q.offer(4);
-//        q.offer(5);
-//        q.offer(6);
-        System.out.println(q.peek());
-        System.out.println(q.size());
-        System.out.println(q.poll());
-//        System.out.println(q.poll());
-        System.out.println(q.peek());
-        System.out.println(q.poll());
+        try {
+            q.offer(1);
+            q.offer(2);
+            q.offer(3);
+            q.offer(4);
+            System.out.println(q.size());
+            q.offer(5);
+            System.out.println(q.size());
+            q.offer(6);
+            System.out.println(q.size());
+            System.out.println(q.peek());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
